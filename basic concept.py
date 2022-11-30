eacc8df5b5f2aaed6b72222464338d0fdc985b66
cshovik <113230439+cshovik@users.noreply.github.com>
2022-11-30 12:54:32 +0530

    Create basic concept.py

---
+++ basic concept.py
@@ -0,0 +1,34 @@
+#Ex -1
+name = "Shovik"
+age = 24
+print(name)
+print(age)
+
+#Ex- 2 boolean type
+first_name = "Tony"
+last_name =  "Stark"
+age = 53
+is_genius = True   
+
+#input type
+name= input("What is your name?")
+print("Hello "+name)
+print("Welcome to home")
+
+#Type Conversion
+old_age = input("Enter your age: ")
+new_age = int(old_age) + 2
+print(new_age)
+
+#useful conversion function
+# 1. float()
+# 2. bool()
+# 3. str()
+# 4 . int()
+
+#code for sum of two number
+first_number = input("Enter first number: ")
+second_number = input("Enter second number: ")
+
+sum = float(first_number) + float(second_number)
+print("The sim is: " + str(sum))
