ae1263a7be1481e67635b5d3c924602600a9808d
cshovik <113230439+cshovik@users.noreply.github.com>
2022-12-01 00:17:20 +0530

    Create Datatype(Tuple,List,index,slicing).py

---
+++ Datatype(Tuple,List,index,slicing).py
@@ -0,0 +1,12 @@
+#concatenation
+mytuple = ('a','b','c','d')
+mytuple1 =  ('d','e')
+mytuple += mytuple1 ## output -a,b,c,d,e
+print(mytuple)
+print(mytuple*2)
+
+#indexing
+print(mytuple[1])
+
+#slicing
+print(mytuple[2:4])
