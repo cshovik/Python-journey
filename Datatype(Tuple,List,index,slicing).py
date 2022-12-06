66ee4f80e86f8cc48b4bcd2470d19835d1ae9d93
cshovik <113230439+cshovik@users.noreply.github.com>
2022-12-06 21:47:38 +0530

    Update Datatype(Tuple,List,index,slicing).py

--- Datatype(Tuple,List,index,slicing).py
+++ Datatype(Tuple,List,index,slicing).py
@@ -10,3 +10,16 @@ print(mytuple[1])
 
 #slicing
 print(mytuple[2:4])
+
+#list
+mylist =['a',123,3,14,'john']
+mylist2 = ["python",20]
+mylist+=mylist2
+print(mylist)
+#append
+mylist.append("10")
+print(mylist) 
+
+#insert
+mylist.insert(1,30)
+print(mylist) # at 1 index 30 is inputed
